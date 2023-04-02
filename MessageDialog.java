2ea950756a024a7c24a759017bb82800d3c375f5
Mubasshir Ahmed Arnab <111133967+mubasshirarnab@users.noreply.github.com>
2023-04-02 23:51:53 +0600

    Add files via upload
    The program will show a simple Message Dialog.

---
+++ MessageDialog.java
@@ -0,0 +1,9 @@
+package DialougeWindow;
+
+import javax.swing.*;
+
+public class MessageDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Wrong Password");
+    }
+}
